e133d34bf3bf4ade757801454662ea0534876735
Sididaoudi <62591474+Sididaoudi@users.noreply.github.com>
2020-03-31 09:36:10 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accueil</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="Css/style.css">
+</head>
+
+<body>
+
+	<img src="img/logo.png">
+
+		<nav>
+
+			<ul>
+
+				<li><a href="index.php">Accueil</a></li>
+				<li><a href="creatures.php">Créatures</a></li>
+				<li><a href="galerie.php">Galerie</a></li>
+				<li><a href="contact.php">Contact</a></li>
+		</nav>
+
+
+
+		</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<footer>
+		  
+		  <a href="mentionslegales.php">Mentions légales</a>
+
+		</footer> 
+
+</body>
+
+
+
+
+
+</html>
