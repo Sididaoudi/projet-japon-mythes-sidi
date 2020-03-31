66c694be65136d2d98e5792473d84cfef73290d1
Sididaoudi <62591474+Sididaoudi@users.noreply.github.com>
2020-03-31 11:16:21 +0200

    up

--- creatures.php
+++ creatures.php
@@ -10,6 +10,33 @@
 		<?php include("nav.php"); ?>
 
 
+	<div class="column">
+
+    <div class="article">
+      <h2>Akamataa</h2>
+      <h5>31 Mars , 2020</h5>
+      <div class="img"><img src="img/1.jpg" width=350px"></div>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+
+    <div class="article">
+      <h2>Akuma<h2>
+      <h5>31 Mars, 2020</h5>
+      <div class="img"><img src="img/.jpg" width=350px"></div>
+      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    </div>
+	
+	</div>
+ 
+    
+   
+ 
+</div>
+
+
+
+
+
 
 
 
